a1590a2def27e833c34f5d4a6f6f84ddef0dab61
SarahNelviandri <sarah.210170236@mhs.unimal.ac.id>
2024-01-09 09:11:05 +0700

    update #4

--- dashboard/welcome/index_admin.php
+++ dashboard/welcome/index_admin.php
@@ -30,27 +30,32 @@ $hak_pengguna = isset($_SESSION['hak']) ? $_SESSION['hak'] : '';
 
 ?>
 
-<?php if ($hak_pengguna === 'admin'): ?>
+<?php if ($hak_pengguna === 'admin') : ?>
     <!-- Tampilkan card hanya jika yang login adalah admin -->
     <div class="d-sm-flex align-items-center justify-content-between mb-5">
         <section class="px-4">
             <div class="d-flex flex-row justify-content-between">
                 <!-- Card Lowongan -->
                 <div class="card text-white bg-secondary mb-3" style="width: 18rem; height: 8rem;">
-                    <div class="card-body">
-                        <h2 class="card-title font-weight-bold">Lowongan</h2>
-                        <h3 class="font-weight-bold"><?php echo $total_lowongan; ?></h3>
-                    </div>
+                    <a href='?r=lowongan/index' style="text-decoration: none; color: inherit;">
+                        <div class="card-body">
+                            <h2 class="card-title font-weight-bold">Lowongan</h2>
+                            <h3 class="font-weight-bold"><?php echo $total_lowongan; ?></h3>
+                        </div>
+                    </a>
                 </div>
+
 
                 <!-- Card Calon Pelamar -->
                 <div style="padding-left: 2%;">
                     <div class="card text-white bg-secondary mb-3" style="width: 18rem; height: 8rem;">
-                        <div class="card-body">
-                            <h2 class="card-title font-weight-bold">Calon Pelamar</h2>
-                            <h3 class="font-weight-bold"><?php echo $total_pendaftaran; ?></h3>
-                        </div>
-                    </div>
+                        <a href='?r=konfirmasi_status_lamaran/index' style="text-decoration: none; color: inherit;">
+                            <div class="card-body">
+                                <h2 class="card-title font-weight-bold">Calon Pelamar</h2>
+                                <h3 class="font-weight-bold"><?php echo $total_pendaftaran; ?></h3>
+                            </div>
+                    
+                    </a>
                 </div>
             </div>
         </section>
